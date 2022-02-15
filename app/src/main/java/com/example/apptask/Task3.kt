ba8ea4f878f458d8e01511b1b7fca77b615f144d
User <mernaeltorke873@gmail.com>
2022-02-15 14:35:18 +0200

    edit1

---
+++ app/src/main/java/com/example/apptask/Task3.kt
@@ -0,0 +1,11 @@
+package com.example.apptask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Task3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task3)
+    }
+}
