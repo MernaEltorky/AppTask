06be39453a98b386e22673291909ff732c4ac5bc
User <mernaeltorke873@gmail.com>
2022-02-23 01:27:54 +0200

    project

--- app/src/main/java/com/example/apptask/tablayout/PagerAdapter.kt
+++ app/src/main/java/com/example/apptask/tablayout/PagerAdapter.kt
@@ -2,9 +2,8 @@ package com.example.apptask.tablayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-@Suppress("DEPRECATION")
 
-internal  class PagerAdapter(var context: FragmentManager, fm: TabLayoutActivity, var totalTabs: Int) :FragmentPagerAdapter(fm)  {
+internal  class PagerAdapter(var context: FragmentManager, fm: TabLayoutActivity, var totalTabs: Int) :FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
